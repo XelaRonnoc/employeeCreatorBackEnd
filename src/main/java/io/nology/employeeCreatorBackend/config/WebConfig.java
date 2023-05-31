30f261c3d86fbd7fee40ccb71118d0cf80bb3e63
Alex <alexcmilton@gmail.com>
2023-05-31 11:34:17 +1000

    fixed bug related to incorrect CORS Mapping configuration

--- src/main/java/io/nology/employeeCreatorBackend/config/WebConfig.java
+++ src/main/java/io/nology/employeeCreatorBackend/config/WebConfig.java
@@ -1,14 +1,16 @@
 package io.nology.employeeCreatorBackend.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer{
 
-	
-	public void addCorsMapping(CorsRegistry registry){
+	@Override
+	public void addCorsMappings(CorsRegistry registry){
 		// create an array of allowed addresses
-		String[] allowedOrigins = {"",""}; // add allowed addresses like local host both local host 5173 and local host 3000
+		String[] allowedOrigins = {"http://127.0.0.1:5173/", "http://localhost:5173/", "http://localhost:3000/"}; // add allowed addresses like local host both local host 5173 and local host 3000
 		registry.addMapping("/**")
 		.allowedOrigins(allowedOrigins)
 		.allowedMethods("GET", "POST", "DELETE")
